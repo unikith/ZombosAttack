f2697be282b13402a2dfb6b27045738567dc0d25
PenguinShade <PenguinShade@Gmail.com>
2017-04-19 20:29:39 -0700

    Add files via upload
    Fixed movement in controller, made gun class, modified player and a little bit of main.

---
+++ Zombie Survival/Zombie Survival/Player.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include "Character.h"
+#include "Gun.h"
+
+using std::string;
+
+class Player : public Character //Maybe inherits from circle and player
+{
+	friend class Controller;
+public:
+	Player(const float& speed, const unsigned int& health);
+	~Player();
+	void update();
+	void attachGun(const string &image, const sf::Vector2f &origin, float fireSpeed,
+		float bulletSpeed, sf::Window *window);
+	void draw(sf::RenderWindow &window);
+	void setTexture(const sf::Texture &texture);
+private:
+	Gun *gun;
+};
